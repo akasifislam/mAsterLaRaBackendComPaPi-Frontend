3cc1e3b7c1f544b5b4f079058d7788ddb2e82387
akasifislam <aseasifislam@gmail.com>
2021-08-13 20:30:33 +0600

    Vue3 Front end with Composition api

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi nihil eveniet, porro debitis ad perspiciatis suscipit alias magnam, laborum asperiores dolorem iste sint cupiditate nisi nostrum eos et esse corrupti?
+  </h1>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
